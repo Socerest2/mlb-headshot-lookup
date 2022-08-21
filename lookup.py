a08eaa42f0ecddcc003a93f1b5fb6370ee1a5ba7
Vince <65380728+Socerest2@users.noreply.github.com>
2022-08-21 00:00:38 -0400

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -7,3 +7,4 @@ print (playerid)
 
 headshot = ("https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/" + playerid + "/headshot/67/" + year)
 print (headshot)
+webbrowser.open_new(headshot)
